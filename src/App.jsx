6382346ec75aef7c5ef871e6bfb1676a46fee80d
HoanNguyen <ndhoan1995@gmail.com>
2023-12-27 10:12:43 +0700

    add theme scheme

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
 import { Alarm, ThreeDRotation } from '@mui/icons-material'
-import { Button } from '@mui/material'
+import { Button, Typography } from '@mui/material'
+import {
+  useColorScheme
+} from '@mui/material/styles'
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
 
 function App() {
   return (
     <>
-      <div>test123</div>
+      <ModeToggle/>
+      <div>danghoandev</div>
+      <Typography variant='body2' color='text.secondary'>danghoandev</Typography>
       <Button variant="contained">Hello World</Button>
       <Alarm />
       <ThreeDRotation />
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import CssBaseline from '@mui/material/CssBaseline'
+// import { ThemeProvider } from '@emotion/react'
+import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material'
+import theme from './theme'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CssBaseline>
-      <App />
-    </CssBaseline>
+    <CssVarsProvider theme={theme}>
+      <CssBaseline>
+        <App />
+      </CssBaseline>
+    </CssVarsProvider>
   </React.StrictMode>
 )
